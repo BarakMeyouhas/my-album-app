eab7a399b2bd02a3d1619b25dd8a02ce17c8932e
Barak1998 <barakm25@gmail.com>
2023-12-03 15:46:49 +0200

    frontend update

--- Frontend/src/Components/Routs/MainRouts/MainRouts.tsx
+++ Frontend/src/Components/Routs/MainRouts/MainRouts.tsx
@@ -4,7 +4,7 @@ import Photos from "../../pages/Photos/Photos";
 import Page404 from "../../pages/Page404/Page404";
 import AddCategory from "../../pages/AddCategory/AddCategory";
 import AddPhoto from "../../pages/AddPhoto/AddPhoto";
-import EditPhoto from "../../pages/EditPhoto/EditPhoto";
+
 
 function MainRoutes(): JSX.Element {
   return (
@@ -14,7 +14,6 @@ function MainRoutes(): JSX.Element {
         <Route path="/addCategory" element={<AddCategory />} />
         <Route path="/cat/:categoryName" element={<Photos />} />
         <Route path="/addPhoto" element={<AddPhoto />} />
-        <Route path="/EditPhoto/:id" element={<EditPhoto />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
